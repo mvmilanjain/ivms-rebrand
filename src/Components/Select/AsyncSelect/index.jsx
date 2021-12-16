9fc9498b7a77a63fbab07db035ae0d6d2f6c3ffb
Milan Jain <mvmilanjain@gmail.com>
2021-12-16 16:45:21 +0530

    Fix for Select and Multi Select

--- src/Components/Select/AsyncSelect/index.jsx
+++ src/Components/Select/AsyncSelect/index.jsx
@@ -1,13 +1,13 @@
-import {useEffect, useState} from 'react';
+import {forwardRef, useEffect, useState} from 'react';
 import isNil from 'lodash/isNil';
 import {Paper, Select, Text} from '@mantine/core';
 import {useDebouncedValue} from '@mantine/hooks';
 
-const LoadingOptions = () => (
+const LoadingOptions = forwardRef((props, ref) => (
     <Paper shadow="sm" padding="sm" withBorder style={{width: '100%'}}>
         <Text size="sm" color="dimmed">Loading options...</Text>
     </Paper>
-);
+));
 
 const AsyncSelect = (
     {
@@ -21,19 +21,16 @@ const AsyncSelect = (
     const [isOpen, toggleOpen] = useState(false);
     const [isLoading, toggleLoading] = useState(false);
     const [options, setOptions] = useState([]);
-    const [selectedItem, setSelectedItem] = useState(selectedValue ? selectedValue.value : '');
-    const [searchText, setSearchText] = useState(selectedValue ? selectedValue.label : '');
+    const [selectedItem, setSelectedItem] = useState(null);
+    const [searchText, setSearchText] = useState('');
     const [debouncedSearchText] = useDebouncedValue(searchText, 300);
 
     useEffect(() => {
-        if (selectedItem) {
-            toggleLoading(true);
-            fetchOptions(debouncedSearchText)
-                .then(options => setOptions(options))
-                .catch(e => setOptions([]))
-                .finally(() => toggleLoading(false));
+        if(!isOpen && selectedValue) {
+            setSelectedItem(selectedValue.value);
+            setOptions([selectedValue]);
         }
-    }, []);
+    }, [selectedValue]);
 
     useEffect(() => {
         if ((isOpen && !isLoading && !options.length) || (isOpen && !isNil(debouncedSearchText))) {
